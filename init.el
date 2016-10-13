aaabb45ffc6960882c4a4966a7ab3de92f71bcfb
Aki Gao <xag9bb@virginia.edu>
2016-10-13 10:16:33 -0700

    updated .gitignore, added tern, js2-mode & neotree, modified web-mode set up

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;
+ ;;
 ;;configures package manager
 ;;
 (require 'package)
@@ -12,10 +12,13 @@
 
 (defvar required-packages
   '(
-    ;;auto-complete,
-    ;;yasnippet,
+    auto-complete
+    tern-auto-complete
+    yasnippet
+    neotree
     web-mode
-    ;;js2-mode
+    js2-mode
+    move-text
     ) "a list of packages to ensure are installed at launch.")
 
 
@@ -37,38 +40,65 @@
     (when (not (package-installed-p p))
       (package-install p))))
 
-;; ;;; yasnippet
-;; ;;; should be loaded before auto complete so that they can work together
-;; (require 'yasnippet)
-;; (yas-global-mode 1)
-
-;; ;;; auto complete mod
-;; ;;; should be loaded after yasnippet so that they can work together
-;; (require 'auto-complete-config)
-;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
-;; (ac-config-default)
-;; ;;; set the trigger key so that it can work together with yasnippet on tab key,
-;; ;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
-;; ;;; activate, otherwise, auto-complete will
-;; (ac-set-trigger-key "TAB")
-;; (ac-set-trigger-key "<tab>");
-
 ;;
 ;;enables and configures web-mode
 ;;
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.scss?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . web-mode))
 (setq web-mode-content-types-alist
-      '(("jsx" . "\\.js[x]?\\'")))
+      '(("html" . "\\.js[x]?\\'"))) 
 
 ;;
 ;;enables and configures js2-mode
 ;;
-;; (add-hook 'js-mode-hook 'js2-minor-mode)
-;; (add-hook 'js2-mode-hook 'ac-js2-mode)
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(setq js2-highlight-level 3)
+
+;;; yasnippet
+;;; should be loaded before auto complete so that they can work together
+(require 'yasnippet)
+(yas-global-mode 1)
+
+;;; auto complete mod
+;;; should be loaded after yasnippet so that they can work together
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(ac-config-default)
+;;; set the trigger key so that it can work together with yasnippet on tab key,
+;;; if the word exists in yasnippet, pressing tab will cause yasnippet to
+;;; activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>");
+
+;;
+;;configures tern
+;;
+(add-to-list 'load-path "/usr/local/bin/tern/emacs")
+(autoload 'tern-mode "tern.el" nil t)
+;sets tern-mode to js mode of choice
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+;use auto-complete for completions
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+; command to kill tern tern server
+ (defun delete-tern-process ()
+  (interactive)
+  (delete-process "Tern"))(tern-ac-setup)))
+
+;;
+;;configures move-text
+;;
+(require 'move-text)
+((message "message" format-args)ove-text-default-bindings)
+
+;;
+;;configures neotree
+;;
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
 
 ;;
 ;;Don't make backup files 
@@ -117,5 +147,4 @@
 (custom-set-variables
  '(custom-enabled-themes (quote (wombat))))
 (custom-set-faces)
-
 
